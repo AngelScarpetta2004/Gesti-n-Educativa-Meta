a120c23749ac2d9032d250beffcaf273f4d26cc6
Angel Daniel Scarpetta Ruiz <114451660+AngelScarpetta2004@users.noreply.github.com>
2025-03-15 17:36:17 -0500

    arreglo db

--- db/conexion.php
+++ db/conexion.php
@@ -1,17 +1,20 @@
 <?php
-$host = getenv('DB_HOST');
+$host = getenv( 'DB_HOST');
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $sslmode = getenv('DB_SSLMODE');
-$cluster = getenv('DB_CLUSTER');
 
 try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode;options='--cluster=$cluster'", $user, $password);
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode";
+    echo "Intentando conectar a: $dsn <br>";
+
+    $conn = new PDO($dsn, $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa 🎉";
+
+    echo "✅ Conexión exitosa 🎉";
 } catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+    echo "❌ Error de conexión: " . $e->getMessage();
 }
 ?>
